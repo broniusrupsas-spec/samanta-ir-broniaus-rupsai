449efad3797706cc0b36df77db9ee7490c966300
broniusrupsas-spec <broniusrupsas@gmail.com>
2026-06-19 18:08:50 +0300

    Update informacija.js

--- informacija.js
+++ informacija.js
@@ -1,304 +1,298 @@
 const accommodation = [
 
-    /* Viešbutis */
+    /* Svečių namelis 1 */
 
     {
         name: "Violeta Žavoronkienė",
         aliases: ["Violeta"],
-        place: "Viešbutis",
+        place: "Svečių namelis 1",
         room: "1 kambarys"
     },
     {
         name: "Zita Balsienė",
         aliases: ["Zita"],
-        place: "Viešbutis",
+        place: "Svečių namelis 1",
         room: "1 kambarys"
     },
 
     {
-        name: "Uogūtė Rupšienė",
-        aliases: ["Uogūtė", "Uogute"],
-        place: "Viešbutis",
+        name: "Uogutė Rupšienė",
+        aliases: ["Uogutė"],
+        place: "Svečių namelis 1",
         room: "2 kambarys"
     },
     {
-        name: "Broniuslovas Rupšas",
-        aliases: ["Broniuslovas"],
-        place: "Viešbutis",
+        name: "Bronislovas Rupšas",
+        aliases: ["Bronislovas"],
+        place: "Svečių namelis 1",
         room: "2 kambarys"
     },
 
     {
         name: "Daiva Paškevičienė",
         aliases: ["Daiva Paškevičienė"],
-        place: "Viešbutis",
+        place: "Svečių namelis 1",
         room: "3 kambarys"
     },
     {
         name: "Nerijus Paškevičius",
         aliases: ["Nerijus Paškevičius"],
-        place: "Viešbutis",
+        place: "Svečių namelis 1",
         room: "3 kambarys"
     },
 
     {
         name: "Viktoras Rupšas",
         aliases: ["Viktoras"],
-        place: "Viešbutis",
+        place: "Svečių namelis 1",
         room: "4 kambarys"
     },
     {
         name: "Laura Balsienė",
         aliases: ["Laura"],
-        place: "Viešbutis",
+        place: "Svečių namelis 1",
         room: "4 kambarys"
     },
 
     {
         name: "Gabija Būtėnaitė",
         aliases: ["Gabija Būtėnaitė"],
-        place: "Viešbutis",
+        place: "Svečių namelis 1",
         room: "5 kambarys"
     },
     {
         name: "Meida Mikalaikevičiūtė",
         aliases: ["Meida"],
-        place: "Viešbutis",
+        place: "Svečių namelis 1",
         room: "5 kambarys"
     },
     {
         name: "Gabija Zajančkauskaitė",
         aliases: ["Gabija Zajančkauskaitė"],
-        place: "Viešbutis",
+        place: "Svečių namelis 1",
         room: "5 kambarys"
     },
     {
         name: "Patricija Markevičiūtė",
         aliases: ["Patricija Markevičiūtė"],
-        place: "Viešbutis",
+        place: "Svečių namelis 1",
         room: "5 kambarys"
     },
     {
         name: "Aušrinė Jokubauskaitė",
         aliases: ["Aušrinė"],
-        place: "Viešbutis",
+        place: "Svečių namelis 1",
         room: "5 kambarys"
     },
 
     {
         name: "Karolis Rupšas",
         aliases: ["Karolis"],
-        place: "Viešbutis",
+        place: "Svečių namelis 1",
         room: "6 kambarys"
     },
     {
         name: "Renata Bri",
         aliases: ["Renata"],
-        place: "Viešbutis",
+        place: "Svečių namelis 1",
         room: "6 kambarys"
     },
 
     {
         name: "Samanta Žavoronkytė",
         aliases: ["Samanta"],
-        place: "Viešbutis",
+        place: "Svečių namelis 1",
         room: "7 kambarys"
     },
     {
         name: "Bronius Rupšas",
         aliases: ["Bronius"],
-        place: "Viešbutis",
+        place: "Svečių namelis 1",
         room: "7 kambarys"
-    },
-
-
-    /* Svečių namelis */
-
-    {
-        name: "Kamilė Pabrėžienė",
-        aliases: ["Kamilė Pabrėžienė"],
-        place: "Svečių namelis",
-        room: "1 kambarys"
-    },
-    {
-        name: "Kazimieras Pabrėžis",
-        aliases: ["Kazimieras"],
-        place: "Svečių namelis",
-        room: "1 kambarys"
-    },
-
-    {
-        name: "Viktorija Linkevičiūtė",
-        aliases: ["Viktorija Linkevičiūtė"],
-        place: "Svečių namelis",
-        room: "2 kambarys"
-    },
-    {
-        name: "Paulius Bastys",
-        aliases: ["Paulius Bastys"],
-        place: "Svečių namelis",
-        room: "2 kambarys"
-    },
-
-    {
-        name: "Ema Girždytė",
-        aliases: ["Ema"],
-        place: "Svečių namelis",
-        room: "3 kambarys"
-    },
-    {
-        name: "Edvinas Stirbys",
-        aliases: ["Edvinas"],
-        place: "Svečių namelis",
-        room: "3 kambarys"
-    },
-
-    {
-        name: "Indrė Klevinskaitė",
-        aliases: ["Indrė"],
-        place: "Svečių namelis",
-        room: "2 aukštas"
-    },
-    {
-        name: "Neringa Balsytė",
-        aliases: ["Neringa"],
-        place: "Svečių namelis",
-        room: "2 aukštas"
-    },
-    {
-        name: "Rokas Narkus",
-        aliases: ["Rokas Narkus"],
-        place: "Svečių namelis",
-        room: "2 aukštas"
-    },
-    {
-        name: "Paulius Stonkus",
-        aliases: ["Paulius Stonkus"],
-        place: "Svečių namelis",
-        room: "2 aukštas"
-    },
-    {
-        name: "Marius Alšauskis",
-        aliases: ["Marius"],
-        place: "Svečių namelis",
-        room: "2 aukštas"
-    },
-    {
-        name: "Beatričė",
-        aliases: ["Beatričė"],
-        place: "Svečių namelis",
-        room: "2 aukštas"
-    },
-    {
-        name: "Benas Tamašauskas",
-        aliases: ["Benas"],
-        place: "Svečių namelis",
-        room: "2 aukštas"
-    },
-    {
-        name: "Rokas Narkus",
-        aliases: ["Rokas Narkus"],
-        place: "Svečių namelis",
-        room: "2 aukštas"
-    },
-    {
-        name: "Patricija Šarskutė",
-        aliases: ["Patricija Šarskutė"],
-        place: "Svečių namelis",
-        room: "2 aukštas"
-    },
-    {
-        name: "Alvyda",
-        aliases: ["Alvyda"],
-        place: "Svečių namelis",
-        room: "2 aukštas"
-    },
-    {
-        name: "Žygimantas Kontrimas",
-        aliases: ["Žygimantas"],
-        place: "Svečių namelis",
-        room: "2 aukštas"
-    },
-    {
-        name: "Ugnius Paškevičius",
-        aliases: ["Ugnius"],
-        place: "Svečių namelis",
-        room: "2 aukštas"
-    },
-    {
-        name: "Evelina Kaukenaitė",
-        aliases: ["Evelina"],
-        place: "Svečių namelis",
-        room: "2 aukštas"
-    },
-    {
-        name: "Vanesa Kaminskaitė",
-        aliases: ["Vanesa"],
-        place: "Svečių namelis",
-        room: "2 aukštas"
-    },
-    {
-        name: "Nedas Paškevičius",
-        aliases: ["Nedas"],
-        place: "Svečių namelis",
-        room: "2 aukštas"
-    },
-    {
-        name: "Aldas Plankis",
-        aliases: ["Aldas"],
-        place: "Svečių namelis",
-        room: "2 aukštas"
     },
 
 
     /* Svečių namelis 2 */
 
     {
+        name: "Kamilė Pabrėžienė",
+        aliases: ["Kamilė Pabrėžienė"],
+        place: "Svečių namelis 2",
+        room: "1 kambarys"
+    },
+    {
+        name: "Kazimieras Pabrėžis",
+        aliases: ["Kazimieras"],
+        place: "Svečių namelis 2",
+        room: "1 kambarys"
+    },
+
+    {
+        name: "Viktorija Linkevičiūtė",
+        aliases: ["Viktorija Linkevičiūtė"],
+        place: "Svečių namelis 2",
+        room: "2 kambarys"
+    },
+    {
+        name: "Paulius Bastys",
+        aliases: ["Paulius Bastys"],
+        place: "Svečių namelis 2",
+        room: "2 kambarys"
+    },
+
+    {
+        name: "Ema Girždytė",
+        aliases: ["Ema"],
+        place: "Svečių namelis 2",
+        room: "3 kambarys"
+    },
+    {
+        name: "Edvinas Stirbys",
+        aliases: ["Edvinas"],
+        place: "Svečių namelis 2",
+        room: "3 kambarys"
+    },
+
+    {
+        name: "Indrė Klevinskaitė",
+        aliases: ["Indrė"],
+        place: "Svečių namelis 2",
+        room: "2 aukštas"
+    },
+    {
+        name: "Neringa Balsytė",
+        aliases: ["Neringa"],
+        place: "Svečių namelis 2",
+        room: "2 aukštas"
+    },
+    {
+        name: "Rokas Narkus",
+        aliases: ["Rokas Narkus"],
+        place: "Svečių namelis 2",
+        room: "2 aukštas"
+    },
+    {
+        name: "Paulius Stonkus",
+        aliases: ["Paulius Stonkus"],
+        place: "Svečių namelis 2",
+        room: "2 aukštas"
+    },
+    {
+        name: "Marius Alšauskis",
+        aliases: ["Marius"],
+        place: "Svečių namelis 2",
+        room: "2 aukštas"
+    },
+    {
+        name: "Beatričė",
+        aliases: ["Beatričė"],
+        place: "Svečių namelis 2",
+        room: "2 aukštas"
+    },
+    {
+        name: "Benas Tamašauskas",
+        aliases: ["Benas"],
+        place: "Svečių namelis 2",
+        room: "2 aukštas"
+    },
+    {
+        name: "Patricija Šarskutė",
+        aliases: ["Patricija Šarskutė"],
+        place: "Svečių namelis 2",
+        room: "2 aukštas"
+    },
+    {
+        name: "Alvyda",
+        aliases: ["Alvyda"],
+        place: "Svečių namelis 2",
+        room: "2 aukštas"
+    },
+    {
+        name: "Žygimantas Kontrimas",
+        aliases: ["Žygimantas"],
+        place: "Svečių namelis 2",
+        room: "2 aukštas"
+    },
+    {
+        name: "Ugnius Paškevičius",
+        aliases: ["Ugnius"],
+        place: "Svečių namelis 2",
+        room: "2 aukštas"
+    },
+    {
+        name: "Evelina Kaukenaitė",
+        aliases: ["Evelina"],
+        place: "Svečių namelis 2",
+        room: "2 aukštas"
+    },
+    {
+        name: "Vanesa Kaminskaitė",
+        aliases: ["Vanesa"],
+        place: "Svečių namelis 2",
+        room: "2 aukštas"
+    },
+    {
+        name: "Nedas Paškevičius",
+        aliases: ["Nedas"],
+        place: "Svečių namelis 2",
+        room: "2 aukštas"
+    },
+    {
+        name: "Aldas Plankis",
+        aliases: ["Aldas"],
+        place: "Svečių namelis 2",
+        room: "2 aukštas"
+    },
+
+
+    /* Svečių namelis 3 */
+
+    {
         name: "Laurynas Girdžius",
         aliases: ["Laurynas"],
-        place: "Svečių namelis 2",
+        place: "Svečių namelis 3",
         room: "2 aukšto 1 kambarys"
     },
     {
         name: "Mantas Rupšas",
         aliases: ["Mantas"],
-        place: "Svečių namelis 2",
+        place: "Svečių namelis 3",
         room: "2 aukšto 1 kambarys"
     },
     {
         name: "Giedrius Dima",
         aliases: ["Giedrius"],
-        place: "Svečių namelis 2",
+        place: "Svečių namelis 3",
         room: "2 aukšto 1 kambarys"
     },
     {
         name: "Jurga Balsytė",
         aliases: ["Jurga"],
-        place: "Svečių namelis 2",
+        place: "Svečių namelis 3",
         room: "2 aukšto 1 kambarys"
     },
 
     {
         name: "Viktorija Rupšaitė",
         aliases: ["Viktorija Rupšaitė"],
-        place: "Svečių namelis 2",
+        place: "Svečių namelis 3",
         room: "2 aukšto 2 kambarys"
     }
 ];
 
 const info = {
-    viesbutis: `
-        <h2>🏠 Viešbutis</h2>
-        <p>Svečių miegamieji.</p>
+    sveciu1: `
+        <h2>🏠 Svečių namelis 1</h2>
+        <p>Svečių miegamieji kambariai.</p>
     `,
 
     sale: `
         <h2>🏛️ Pokylių salė</h2>
-        <p>Čia persikelsim švęsti po 12 valandos iki kol galėsim.</p>
+        <p>Čia vyks visas veiksmas po 12 valandos iki kol galėsim ir pusryčiai.</p>
     `,
 
-    sveciu1: `
-        <h2>🏠 Svečių namelis</h2>
+    sveciu2: `
+        <h2>🏠 Svečių namelis 2</h2>
         <p>Svečių miegamieji kambariai.</p>
     `,
 
@@ -307,8 +301,8 @@ const info = {
         <p>Čia vyks visa vakarinė dalis iki 12 valandos.</p>
     `,
 
-    sveciu2: `
-        <h2>🏠 Svečių namelis 2</h2>
+    sveciu3: `
+        <h2>🏠 Svečių namelis 3</h2>
         <p>Svečių miegamieji kambariai.</p>
     `
 };
