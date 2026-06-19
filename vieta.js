510097871821aff8a4f791b2c153c160e265838b
broniusrupsas-spec <broniusrupsas@gmail.com>
2026-06-19 18:25:29 +0300

    Update vieta.js

--- vieta.js
+++ vieta.js
@@ -3,81 +3,81 @@ const seating = [
     {
         table: 1,
         guests: [
-            { name: "Aldas Plankis", display: "Aldas", gender: "male" },
-            { name: "Kamilė Mikšionė", display: "Kamilė M.", gender: "female" },
-            { name: "Pijus Mikšionis", display: "Pijus", gender: "male" },
-            { name: "Benas Mockus", display: "Benas M.", gender: "male" },
-            { name: "Diana Mockienė", display: "Diana", gender: "female" },
-            { name: "Gabija Būtėnaitė", display: "Gabija B.", gender: "female" },
-            { name: "Kamile Kvedaraitė", display: "Kamilė K.", gender: "female" },
-            { name: "Julija Tumul", display: "Julija", gender: "female" },
-            { name: "Valerija Valner", display: "Valerija", gender: "female" },
-            { name: "Evelina Kaukėnaitė", display: "Evelina", gender: "female" }
+            { name: "Aldas Plankis", gender: "male" },
+            { name: "Kamilė Mikšionė", gender: "female" },
+            { name: "Pijus Mikšionis", gender: "male" },
+            { name: "Benas Mockus", gender: "male" },
+            { name: "Diana Mockienė", gender: "female" },
+            { name: "Gabija Būtėnaitė", gender: "female" },
+            { name: "Kamile Kvedaraitė", gender: "female" },
+            { name: "Julija Tumul", gender: "female" },
+            { name: "Valerija Valner", gender: "female" },
+            { name: "Evelina Kaukėnaitė", gender: "female" }
         ]
     },
 
     {
         table: 2,
         guests: [
-            { name: "Violeta Žavoronkienė", display: "Violeta", gender: "female" },
-            { name: "Laura Balsienė", display: "Laura", gender: "female" },
-            { name: "Viktoras Rupšas", display: "Viktoras", gender: "male" },
-            { name: "Nerijus Paškevičius", display: "Nerijus", gender: "male" },
-            { name: "Daiva Paškevičienė", display: "Daiva", gender: "female" },
-            { name: "Renata Bit", display: "Renata", gender: "female" },
-            { name: "Karolis Rupšas", display: "Karolis", gender: "male" },
-            { name: "Zita Balsienė", display: "Zita", gender: "female" },
-            { name: "Broniuslovas Rupšas", display: "Broniuslovas", gender: "male" },
-            { name: "Uogutė Rupšienė", display: "Uogutė", gender: "female" }
+            { name: "Violeta Žavoronkienė", gender: "female" },
+            { name: "Laura Balsienė", gender: "female" },
+            { name: "Viktoras Rupšas", gender: "male" },
+            { name: "Nerijus Paškevičius", gender: "male" },
+            { name: "Daiva Paškevičienė", gender: "female" },
+            { name: "Renata Bit", gender: "female" },
+            { name: "Karolis Rupšas", gender: "male" },
+            { name: "Zita Balsienė", gender: "female" },
+            { name: "Bronislovas Rupšas", gender: "male" },
+            { name: "Uogutė Rupšienė", gender: "female" }
         ]
     },
 
     {
         table: 3,
         guests: [
-            { name: "Meida Mikalaikevičiūtė", display: "Meida", gender: "female" },
-            { name: "Samanta Žavoronkytė", display: "Samanta", gender: "female" },
-            { name: "Bronius Rupšas", display: "Bronius", gender: "male" },
-            { name: "Edvinas Stirbys", display: "Edvinas", gender: "male" },
-            { name: "Ema Girždytė", display: "Ema", gender: "female" },
-            { name: "Vanesa Kaminskaitė", display: "Vanesa", gender: "female" },
-            { name: "Žygimantas Kontrimas", display: "Žygis", gender: "male" },
-            { name: "Paulius Stonkus", display: "Paulius S.", gender: "male" },
-            { name: "Indrė Klevinskaitė", display: "Indrė", gender: "female" },
-            { name: "Gabija Zajanckauskaitė", display: "Gabija", gender: "female" },
-            { name: "Patricija Markevičiūtė", display: "Patricija", gender: "female" }
+            { name: "Meida Mikalaikevičiūtė", gender: "female" },
+            { name: "Samanta Žavoronkytė", gender: "female" },
+            { name: "Bronius Rupšas", gender: "male" },
+            { name: "Edvinas Stirbys", gender: "male" },
+            { name: "Ema Girždytė", gender: "female" },
+            { name: "Vanesa Kaminskaitė", gender: "female" },
+            { name: "Žygimantas Kontrimas", gender: "male" },
+            { name: "Paulius Stonkus", gender: "male" },
+            { name: "Indrė Klevinskaitė", gender: "female" },
+            { name: "Gabija Zajanckauskaitė", gender: "female" },
+            { name: "Patricija Markevičiūtė", gender: "female" }
         ]
     },
 
     {
         table: 4,
         guests: [
-            { name: "Kazimieras Pabiržis", display: "Kazimieras", gender: "male" },
-            { name: "Kamile Pabiržienė", display: "Kamilė P.", gender: "female" },
-            { name: "Aušrinė Jokubauskaitė", display: "Aušrinė", gender: "female" },
-            { name: "Giedrius Dima", display: "Giedrius", gender: "male" },
-            { name: "Rokas Narkus", display: "Rokas", gender: "male" },
-            { name: "Beatričė", display: "Beatričė", gender: "female" },
-            { name: "Dovydas Narkus", display: "Dovydas", gender: "male" },
-            { name: "Alvyda", display: "Alvyda", gender: "female" },
-            { name: "Viktorija Linkevičiūtė", display: "Viktorija L.", gender: "female" },
-            { name: "Paulius Bastys", display: "Paulius B.", gender: "male" },
-            { name: "Patricija Šarskutė", display: "Patricija Š.", gender: "female" },
-            { name: "Benas Tamaušauskas", display: "Benas T.", gender: "male" }
+            { name: "Kazimieras Pabiržis", gender: "male" },
+            { name: "Kamile Pabiržienė", gender: "female" },
+            { name: "Aušrinė Jokubauskaitė", gender: "female" },
+            { name: "Giedrius Dima", gender: "male" },
+            { name: "Rokas Narkus", gender: "male" },
+            { name: "Beatričė", gender: "female" },
+            { name: "Dovydas Narkus", gender: "male" },
+            { name: "Alvyda", gender: "female" },
+            { name: "Viktorija Linkevičiūtė", gender: "female" },
+            { name: "Paulius Bastys", gender: "male" },
+            { name: "Patricija Šarskutė", gender: "female" },
+            { name: "Benas Tamaušauskas", gender: "male" }
         ]
     },
 
     {
         table: 5,
         guests: [
-            { name: "Mantas Rupšas", display: "Mantas", gender: "male" },
-            { name: "Laurynas Girdžius", display: "Laurynas", gender: "male" },
-            { name: "Viktorija Rupšaitė", display: "Viktorija R.", gender: "female" },
-            { name: "Jurga Balsytė", display: "Jurga", gender: "female" },
-            { name: "Neringa Balsytė", display: "Neringa", gender: "female" },
-            { name: "Marius Alšauskis", display: "Marius", gender: "male" },
-            { name: "Ugnius Paškevičius", display: "Ugnius", gender: "male" },
-            { name: "Nedas Paškevičius", display: "Nedas", gender: "male" }
+            { name: "Mantas Rupšas", gender: "male" },
+            { name: "Laurynas Girdžius", gender: "male" },
+            { name: "Viktorija Rupšaitė", gender: "female" },
+            { name: "Jurga Balsytė", gender: "female" },
+            { name: "Neringa Balsytė", gender: "female" },
+            { name: "Marius Alšauskis", gender: "male" },
+            { name: "Ugnius Paškevičius", gender: "male" },
+            { name: "Nedas Paškevičius", gender: "male" }
         ]
     }
 ];
@@ -109,8 +109,8 @@ function renderTables(){
         tableBlock.appendChild(circle);
 
         const total = tableData.guests.length;
-        const startAngle = -145;
-        const endAngle = 145;
+        const startAngle = -155;
+        const endAngle = 155;
         const step = total === 1 ? 0 : (endAngle - startAngle) / (total - 1);
 
         tableData.guests.forEach((guest, index) => {
@@ -119,17 +119,11 @@ function renderTables(){
             const guestEl = document.createElement("div");
             guestEl.className = "guest";
             guestEl.style.setProperty("--angle", `${angle}deg`);
-            guestEl.style.setProperty("--text-angle", `${angle / 2}deg`);
-
-            const nameEl = document.createElement("div");
-            nameEl.className = "guest-name";
-            nameEl.innerText = guest.display;
 
             const avatar = document.createElement("div");
             avatar.className = `avatar ${guest.gender}`;
-            avatar.innerText = guest.gender === "male" ? "👤" : "👤";
+            avatar.innerText = guest.gender === "male" ? "♙" : "♙";
 
-            guestEl.appendChild(nameEl);
             guestEl.appendChild(avatar);
             tableBlock.appendChild(guestEl);
         });
@@ -171,11 +165,6 @@ function findSeat(){
 
     if(exactFullMatches.length === 1){
         showSeatResult(exactFullMatches[0]);
-        return;
-    }
-
-    if(exactFullMatches.length > 1){
-        result.innerHTML = "Rasta daugiau nei vienas žmogus. Įveskite vardą ir pavardę.";
         return;
     }
 
